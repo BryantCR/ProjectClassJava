37f512b7461f2f4d7f27db368aad0570c3e703b3
BryantCR <bryancvargas20@gmail.com>
2021-10-19 11:57:44 -0600

    ok

--- ProjectClass.java
+++ ProjectClass.java
@@ -1,3 +1,41 @@
-public class ProjectClass(){
-    
+
+
+public class ProjectClass{
+    // Attributes
+    private String name;
+    private String description;
+
+    public ProjectClass( String name, String description ){
+        this.name = name;
+        this.description = description;
+    }
+
+    // Constructor overloaded
+    public ProjectClass( String name ){
+        this.name = name;
+    }
+
+    // Getters
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    // Setters
+    public void setName( String name ){
+        this.name = name;
+    }
+
+    public void setDescription( String description ){
+        this.description = description;
+    }
+
+    public void print(){
+        System.out.println( "Name: " + this.name );
+        System.out.println( "Description: " + this.description );    
+    }
+
 }
--- ProjectClassTest.java
+++ ProjectClassTest.java
@@ -1,3 +1,18 @@
 public class ProjectClassTest {
     
+    public static void name( String args[] ){
+
+        ProjectClass solarSystem = new ProjectClass("Solar System", "The solar system is the sun and everything that orbits around it.");
+        ProjectClass lavaVolcano = new ProjectClass("Lava Volcano", "Lava, magma (molten rock) emerging as a liquid onto Earth's surface.");
+        
+        System.out.println("Project Tittle: "+ solarSystem.getName());
+        System.out.println("Project Description: "+ solarSystem.getDescription());
+        solarSystem.print();
+        System.out.println("*============================*");
+        System.out.println("Project Tittle: "+ lavaVolcano.getName());
+        System.out.println("Project Description: "+ lavaVolcano.getDescription());
+        lavaVolcano.print();
+
+    }
+
 }
